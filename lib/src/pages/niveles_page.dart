989a4f7f5d23562bed0912045708210d87c06511
Carlos Manuel Sanchez Martinez <43074684+Carlos5anchez@users.noreply.github.com>
2019-11-19 13:58:10 -0600

    aaa

---
+++ lib/src/pages/niveles_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class Niveles extends StatelessWidget {
+  const Niveles({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+      final medida=MediaQuery.of(context).size;
+    return Scaffold(
+      body:Text('data') ,
+    );
+
+  }
+}
