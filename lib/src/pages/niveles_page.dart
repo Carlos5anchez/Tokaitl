8de41c686565b9b42484eed41b384fd13e19560b
Ahnklex <40051739+Mahnkey@users.noreply.github.com>
2019-11-24 22:25:52 -0600

    niveles

--- lib/src/pages/niveles_page.dart
+++ lib/src/pages/niveles_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_container/responsive_container.dart';
 
 
 class Niveles extends StatelessWidget {
@@ -7,9 +8,126 @@ class Niveles extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
       final medida=MediaQuery.of(context).size;
-    return Scaffold(
-      body:Text('data') ,
-    );
+    return new MaterialApp(
+      home:Scaffold(
+        body: Center(
+        child: ResponsiveContainer(
+          heightPercent: 70,
+          widthPercent: 85,
+          child: Container(
+          decoration: new BoxDecoration(
+            border: Border.all(
+              color: Colors.white,
+              width: 2,
+              
+            ),
+            borderRadius: BorderRadius.all(
+              Radius.circular(30)
+            )
+          ),
+            child:Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Container(
+                      height: 100,
+                      width: 70,
+                      decoration: new BoxDecoration(
+                          color: Colors.amber,
+                            image: DecorationImage(
+                              image: new AssetImage('assets/candado.png'),
+                                fit: BoxFit.fitWidth,
+                                ),
+                              shape: BoxShape.circle
+                              
+                    ),
+                    ),
+                    Container(
+                      height: 100,
+                      width: 70,
+                      decoration: new BoxDecoration(
+                          color: Colors.amber,
+                            image: DecorationImage(
+                              image: new AssetImage('assets/candado.png'),
+                                fit: BoxFit.fitWidth,
+                                ),
+                              shape: BoxShape.circle
+                              
+                    ),
+                    ),
+                    Container(
+                      height: 100,
+                      width: 70,
+                      decoration: new BoxDecoration(
+                          color: Colors.amber,
+                            image: DecorationImage(
+                              image: new AssetImage('assets/candado.png'),
+                                fit: BoxFit.fitWidth,
+                                ),
+                              shape: BoxShape.circle
+                              
+                    ),
+                    ),
+                   
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Container(
+                      height: 100,
+                      width: 70,
+                      decoration: new BoxDecoration(
+                            color: Colors.amber,
+                            image: DecorationImage(
+                              image: new AssetImage('assets/candado.png'),
+                                fit: BoxFit.fitWidth,
+                                ),
+                              shape: BoxShape.circle
+                    ),
+                    ),
+                    Container(
+                      height: 100,
+                      width: 70,
+                      decoration: new BoxDecoration(
+                            color: Colors.amber,
+                            image: DecorationImage(
+                              image: new AssetImage('assets/candado.png'),
+                                fit: BoxFit.fitWidth,
+                                ),
+                              shape: BoxShape.circle
+                    ),
+                    ),
+                    Container(
+                      height: 100,
+                      width: 70,
+                      decoration: new BoxDecoration(
+                            color: Colors.amber,
+                            image: DecorationImage(
+                              image: new AssetImage('assets/candado.png'),
+                                fit: BoxFit.fitWidth,
+                                ),
+                              shape: BoxShape.circle
+                    ),
+                    )  
+                  ],
+                ),
+                 
 
+            
+              ],
+          ),
+
+        )
+        )
+        ),
+      
+
+      )
+      
+    );
+    
   }
 }
